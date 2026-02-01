44117b95555e099048cbfd0549b2aa243cab001c
zivawernick <22984777+zivawernick@users.noreply.github.com>
2026-02-01 18:59:54 +0200

    work to fix theme controler

--- src/settings.ts
+++ src/settings.ts
@@ -23,7 +23,7 @@ export const template = {
 	website_url: 'https://localhost:4321', // Astro needs to know your site’s deployed URL to generate a sitemap. It must start with http:// or https://
 	menu_left: false,
 	transitions: true,
-	lightTheme: 'light', // Select one of the Daisy UI Themes or create your own
+	lightTheme: 'corporate', // Select one of the Daisy UI Themes or create your own
 	darkTheme: 'forest', // Select one of the Daisy UI Themes or create your own
 	excerptLength: 200,
 	postPerPage: 5,
