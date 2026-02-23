8a5b498ff91d07b31870478f3f667723aac3cb79
zivawernick <22984777+zivawernick@users.noreply.github.com>
2026-02-23 20:21:36 +0200

    fix: sitemap and package name

--- src/settings.ts
+++ src/settings.ts
@@ -18,7 +18,7 @@ export const social = {
 }
 
 export const template = {
-	website_url: 'https://localhost:4321', // Astro needs to know your site’s deployed URL to generate a sitemap. It must start with http:// or https://
+	website_url: 'https://gene.rabbiwernick.com', // Astro needs to know your site’s deployed URL to generate a sitemap. It must start with http:// or https://
 	menu_left: false,
 	transitions: true,
 	lightTheme: 'corporate', // Select one of the Daisy UI Themes or create your own
